d681f4af0614865ed532a046ec0a10c8615f3899
Simon Sinding <local@sinding>
2026-03-04 14:58:16 +0100

    feat: auto expand nvim tree

--- nvim/lua/custom/plugins/nvim-tree.lua
+++ nvim/lua/custom/plugins/nvim-tree.lua
@@ -11,7 +11,24 @@ return {
 
     -- optionally enable 24-bit colour
     vim.opt.termguicolors = true
-    require('nvim-tree').setup {}
 
+    require('nvim-tree').setup {
+      view = { adaptive_size = true },
+      on_attach = function(bufnr)
+        local api = require 'nvim-tree.api'
+
+        -- First load all default mappings
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- Then override the cd mapping
+        vim.keymap.set('n', '<C-o>', api.tree.change_root_to_node, {
+          buffer = bufnr,
+          noremap = true,
+          silent = true,
+          nowait = true,
+          desc = 'nvim-tree: CD',
+        })
+      end,
+    }
   end,
 }
